be7adf707f50bd9561348773811b41b7b04e9639
Rachel <raitkens724@gmail.com>
2020-03-29 10:16:38 -0500

    finish stuff

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -13,22 +13,23 @@ public abstract class JobField {
     }
 
     public JobField() {
-
+        id = nextId;
+        nextId++;
     }
 
     // Custom toString, equals, and hashCode methods:
-
+    @Override
     public String toString() {
         return value;
     }
-
+    @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
         if (this == o) return true;
         if (!(o instanceof JobField)) return false;
         JobField jobField = (JobField) o;
         return getId() == jobField.getId();
     }
-
+    @Override
     public int hashCode() {
         return Objects.hash(getId());
     }
